24f95bc0f9f725e559d9e7d059b9b814297b870c
cking945 <collierking99@gmail.com>
2017-12-25 15:30:10 -0600

    12252017

---
+++ SQL/Oracle/NVL.sql
@@ -0,0 +1,7 @@
+NVL
+-- Description
+-- The Oracle/PLSQL NVL function lets you substitute a value when a null value is encountered.
+-- Syntax
+-- NVL( string1, replace_with )
+-- http://sqlfiddle.com/#!4/4163d/1
+
---
+++ SQL/Oracle/variables.sql
@@ -0,0 +1,67 @@
+--LAB1
+DECLARE
+v_name varchar2(20) := 'Collier King';
+BEGIN
+dbms_output.put_line(v_name);
+END;
+/
+
+--LAB2
+--EXAMPLE 1 (1 variable)
+DECLARE
+
+v_number NUMBER := 7369;
+v_sal    emp.sal%TYPE;
+
+BEGIN
+
+select sal
+into v_sal
+from emp
+where empno = v_number;
+
+v_sal := v_sal * 1.1;
+
+dbms_output.put_line('Employee ' || v_number || ' New Sal ' || v_sal);
+
+END;
+/
+
+--EXAMPLE 2 (2 variables)
+
+DECLARE
+
+emp_rec  emp%ROWTYPE;
+v_number NUMBER := 7369;
+
+BEGIN
+
+select *
+into emp_rec
+from emp
+where empno = v_number;
+
+dbms_output.put_line('New Salary for  ' || emp_rec.ename || ' is ' || emp_rec.sal * 1.1);
+
+END;
+/
+
+--EXAMPLE 3 (3 variables)
+
+DECLARE
+
+v_number NUMBER := 7369;
+v_ename  emp.ename%TYPE;
+v_sal    emp.sal%TYPE;
+
+BEGIN
+
+select ename, sal * 1.1
+into v_ename, v_sal
+from emp
+where empno = v_number;
+
+dbms_output.put_line('New Salary for  ' || n_ename || ' is ' || v_sal);
+
+END;
+/
